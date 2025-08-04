d8c5afd8d2c9741a84212657306d50bc49d36281
Suraj Kumar <surajxcode@icloud.com>
2025-08-04 23:04:40 +0530

    Introduction: Output & Variables Variables & Updation of Variables
    Signed-off-by: Suraj Kumar <surajxcode@icloud.com>

---
+++ 01: Introduction/01-Output and Variables/printVaraibles.cpp
@@ -0,0 +1,14 @@
+// Printing Variable in C++ & Updation of Variables
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 5; 
+    cout<<x<<endl; // 5
+    x = 7;
+    cout<<x<<endl; // 7
+    x = x + 6; 
+    cout<<x<<endl; // 13
+    x = x - 20; 
+    cout << x<<endl; // -7
+    return 0;
+}
